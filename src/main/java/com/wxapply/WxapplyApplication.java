67d9301ae44b44cf433b7a3b2afbbffb16114795
zhangjun <136841189@qq.com>
2018-11-01 17:05:37 +0800

    init project

---
+++ src/main/java/com/wxapply/WxapplyApplication.java
@@ -0,0 +1,12 @@
+package com.wxapply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxapplyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxapplyApplication.class, args);
+    }
+}
