5a2dbedbe0bcc862fca87ae50eb5dde9e79325dc
zhangjun <136841189@qq.com>
2018-11-05 14:55:48 +0800

    phase 1 微调

--- src/main/java/com/wxapply/controller/WxController.java
+++ src/main/java/com/wxapply/controller/WxController.java
@@ -8,10 +8,13 @@ import me.chanjar.weixin.common.bean.WxJsapiSignature;
 import me.chanjar.weixin.common.error.WxErrorException;
 import me.chanjar.weixin.mp.api.WxMpMessageRouter;
 import me.chanjar.weixin.mp.bean.device.WxDeviceMsg;
+import me.chanjar.weixin.mp.bean.kefu.WxMpKefuMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
+import me.chanjar.weixin.mp.bean.result.WxMpQrCodeTicket;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateData;
 import me.chanjar.weixin.mp.bean.template.WxMpTemplateMessage;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,6 +22,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/")
@@ -114,6 +120,52 @@ public class WxController {
         }
 
         return "非法请求";
+    }
+
+    @PostMapping("/sendKefuMessage")
+    public boolean sendKefuMessage() throws WxErrorException {
+        WxMpKefuMessage.WxArticle wxArticle = new WxMpKefuMessage.WxArticle();
+        wxArticle.setDescription("无锡市滨湖区地处长江三角洲腹地，江苏省东南部，无锡市西南部。南依太湖，北靠长江，东临上海，西接南京。全区区域面积628.15平方公里，其中陆地面积257.89平方公里，太湖湖岸线82.53公里，常住人口约69.5万，是长三角地区具有较大影响力和辐射力的生态新城、科创高地、文旅之都和商务中心。");
+        wxArticle.setPicUrl("https://mmbiz.qpic.cn/mmbiz_png/yOTDWHboj5FfRH5ibFYtvFALFL42n0CW61kXPS19xf73W5rXTKRCX1crZJib6BicamD0eQF1AibljDSoDDBh1LAoEA/640?wx_fmt=png&tp=webp&wxfrom=5&wx_lazy=1&wx_co=1");
+        wxArticle.setTitle("醉美湖湾·无锡滨湖欢迎您来创新创业");
+        wxArticle.setUrl("https://mp.weixin.qq.com/s?__biz=MzU0NjkyNzEyNg==&mid=100000426&idx=1&sn=a528698942e61cb82f68fc052d363d53&scene=19#wechat_redirect");
+
+        WxMpKefuMessage wxMpKefuMessage = WxMpKefuMessage.NEWS()
+                .toUser(openId)
+                .addArticle(wxArticle)
+                .build();
+
+        boolean isTrue = wxService.getKefuService().sendKefuMessage(wxMpKefuMessage);
+        return isTrue;
+    }
+
+    @PostMapping("/getQrCode")
+    public String getQrCode() throws WxErrorException, IOException {
+        int scene = 121;
+        Integer expireSeconds = 7200;
+        WxMpQrCodeTicket ticket = wxService.getQrcodeService().qrCodeCreateTmpTicket(scene, expireSeconds);
+        // 获得在系统临时目录下的文件，需要自己保存使用，注意：临时文件夹下存放的文件不可靠，不要直接使用
+        File file = wxService.getQrcodeService().qrCodePicture(ticket);
+
+        String qrFilePath = System.getProperty("user.dir") + "\\src\\main\\resources\\qrCode\\";
+        String fileName = file.getName();
+        File file1 = new File(qrFilePath);
+        File file2 = new File(qrFilePath + fileName);
+        if (!file1.exists() && file1.mkdirs() && file2.createNewFile()) {
+            FileOutputStream fileOutputStream = new FileOutputStream(file2);
+            fileOutputStream.write(FileUtils.readFileToByteArray(file));
+            System.out.println("create success!");
+        }else {
+            FileOutputStream fileOutputStream = new FileOutputStream(file2);
+            fileOutputStream.write(FileUtils.readFileToByteArray(file));
+            System.out.println("create success!");
+        }
+        return null;
+    }
+
+    @GetMapping("/getShortUrl")
+    public String getShortUrl() throws WxErrorException {
+        return wxService.shortUrl("www.baidu.com");
     }
 
     /**
